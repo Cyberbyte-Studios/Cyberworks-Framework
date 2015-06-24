7c934e5a4f799c6a6414f7ac222fdc6c3a0e7020
MightySCollins <theatrepro11@gmail.com>
2015-06-24 08:15:50 +0100

    English File

---
+++ English.php
@@ -0,0 +1,248 @@
+<?php
+$lang = array();
+
+/*
+------------------------------------
+	Common Items / Header Items
+------------------------------------
+*/
+
+$lang['welcome'] = 'Welcome';
+$lang['players'] = 'Players';
+$lang['player'] = 'Player';
+$lang['vehicles'] = 'Vehicles';
+$lang['vehicle'] = 'Vehicle';
+$lang['houses'] = 'Houses';
+$lang['house'] = 'House';
+$lang['gangs'] = 'Gangs';
+$lang['gang'] = 'Gang';
+$lang['wanted'] = 'Wanted';
+$lang['staff'] = 'Staff';
+$lang['overview'] = 'Overview';
+$lang['search'] = 'Search';
+$lang['edit'] = 'Edit';
+$lang['editing'] = 'Editing';
+$lang['editor'] = 'Editor';
+$lang['members'] = 'Members';
+$lang['subChange'] = 'Submit Changes';
+$lang['quickLook'] = 'Quick Look';
+$lang['curPlayers'] = 'Current Players';
+$lang['statOver'] = 'Statistics Overview';
+$lang['overview'] = 'Overview';
+$lang['viewAll'] = 'View All';
+$lang['subChanges'] = 'Update';
+$lang['yes'] = 'Yes';
+$lang['no'] = 'No';
+$lang['car'] = 'Car';
+$lang['air'] = 'Air';
+$lang['ban'] = 'Ban';
+$lang['ship'] = 'Ship';
+$lang['store'] = 'Store';
+$lang['add'] = 'Add';
+$lang['new'] = 'New';
+$lang['note'] = 'Note';
+$lang['id'] = 'ID';
+$lang['uid'] = 'User ID';
+$lang['page'] = 'Page';
+$lang['not'] = 'Not';
+
+/*
+-----------------
+	Errors
+-----------------
+*/
+$lang['error'] = 'Error';
+$lang['dbError'] = 'Database connection problem.';
+$lang['noPerm'] = 'Sorry you do not have permission to view this page';
+$lang['noMessage'] = 'No messages';
+$lang['noHouse'] = 'No Houses';
+$lang['noGang'] = 'No Gangs';
+$lang['noID'] = 'No ID';
+$lang['noRec'] = 'No records found';
+$lang['noNotes'] = 'No notes for this player';
+$lang['noVeh'] = 'No Vehicles';
+$lang['noPlayer'] = 'Player cannot be found';
+$lang['fillAll'] = 'Please fill everything in';
+$lang['noLang'] = 'Language not allowed';
+$lang['incorrectPass'] = 'Incorrect Password';
+$lang['failPID'] = 'Player ID not found on steam';
+$lang['matchPass'] = 'The passwords must match';
+$lang['notverified'] = 'Verification Failed';
+
+/*
+------------------
+	Menu Items
+------------------
+*/
+
+$lang['navProfile'] = 'Profile';
+$lang['navAdmin'] = 'Admin';
+$lang['navNewUser'] = 'Add New User';
+$lang['navDashboard'] = 'Dashboard';
+$lang['navLogOut'] = 'Log Out';
+$lang['first'] = 'First';
+$lang['previous'] = 'Previous';
+$lang['next'] = 'Next';
+$lang['last'] = 'Last';
+$lang['editPlayer'] = 'Edit Player';
+
+/*
+-------------------------------
+	Table Headers / Buttons
+-------------------------------
+*/
+
+$lang['staffName'] = 'Staff Name';
+$lang['emailAdd'] = 'Email Address';
+$lang['rank'] = 'Rank';
+$lang['name'] = 'Name';
+$lang['wantList'] = 'Wanted List';
+$lang['aliases'] = 'Aliases';
+$lang['owner'] = 'Owner';
+$lang['maxMembers'] = 'Max Members';
+$lang['members'] = 'Members';
+$lang['bank'] = 'Bank';
+$lang['cash'] = 'Cash';
+$lang['cop'] = 'Cop';
+$lang['police'] = 'Police';
+$lang['medic'] = 'Medic';
+$lang['medics'] = 'Medics';
+$lang['civil'] = 'Civilian';
+$lang['civ'] = 'Civ';
+$lang['side'] = 'Side';
+$lang['admin'] = 'Admin';
+$lang['donator'] = 'Donator';
+$lang['licenses'] = 'Licenses';
+$lang['gear'] = 'Gear';
+$lang['active'] = 'Active';
+$lang['playerID'] = 'Player ID';
+$lang['GUID'] = 'GUID';
+$lang['position'] = 'Position';
+$lang['owned'] = 'Owned';
+$lang['inventory'] = 'Inventory';
+$lang['containers'] = 'Containers';
+$lang['class'] = 'Class';
+$lang['type'] = 'Type';
+$lang['plate'] = 'Plate';
+$lang['alive'] = 'Alive';
+$lang['colour'] = 'Colour';
+$lang['class'] = 'Class';
+$lang['uid'] = 'uID';
+
+/*
+---------------------------
+	SQL Phone Module
+---------------------------
+*/
+$lang['sqlPhone'] = 'SQL Smart Phone';
+$lang['from'] = 'From';
+$lang['to'] = 'To';
+$lang['message'] = 'Message';
+$lang['time'] = 'Time';
+$lang['messages'] = 'Messages';
+
+/*
+---------------------------
+	Plugin Store
+---------------------------
+*/
+$lang['pluginstore'] = 'Plugin Store';
+$lang['visitSite'] = 'Visit Site';
+$lang['installed'] = 'Installed';
+$lang['updateAvalible'] = 'Update Available';
+$lang['update'] = 'Update';
+$lang['installed'] = 'Installed';
+$lang['install'] = 'Install';
+$lang['active'] = 'Active';
+$lang['activate'] = 'Activate';
+$lang['deactivate'] = 'Deactivate';
+$lang['delete'] = 'Delete';
+
+/*
+--------------------------
+	Wanted
+--------------------------
+*/
+$lang['wID'] = 'Wanted ID';
+$lang['bounty'] = 'Bounty';
+$lang['crimes'] = 'Crimes';
+
+/*
+--------------------------
+	Settings
+--------------------------
+*/
+$lang['key'] = 'Key';
+$lang['registration'] = 'Allow user registration';
+$lang['steamlogin'] = 'Allow login with steam';
+$lang['useNews'] = 'Use CyberByte News Feed';
+$lang['panel'] = 'Panel';
+$lang['useVAC'] = 'Check VAC Bans';
+$lang['itemsPP'] = 'Items Per Page';
+$lang['settings'] = 'Settings';
+$lang['allowLang'] = 'Allow other languages';
+
+/*
+--------------------------
+	Logs
+--------------------------
+*/
+$lang['failedAccess'] = 'Failed to access';
+$lang['logs'] = 'Logs';
+$lang['log'] = 'Log';
+$lang['searched'] = 'Searched';
+$lang['deleted'] = 'Deleted';
+$lang['id'] = 'ID';
+$lang['time'] = 'Time';
+$lang['action'] = 'Action';
+$lang['level'] = 'Level';
+
+/*
+--------------------------
+	Misc
+--------------------------
+*/
+$lang['setup'] = 'The database has now been setup';
+$lang['upgrade'] = 'The database has now been upgraded';
+$lang['Kick'] = 'Kick';
+$lang['enterPass'] = 'Enter new password';
+$lang['password'] = 'Password';
+$lang['repeat'] = 'Repeat';
+$lang['login'] = 'Login';
+$lang['cancel'] = 'Cancel';
+$lang['picture'] = 'Picture';
+$lang['changePass'] = 'Change Password';
+$lang['gameServers'] = 'Game Servers';
+$lang['blacklisted'] = 'Blacklisted';
+$lang['arrested'] = 'Arrested';
+$lang['finances'] = 'Finances';
+$lang['username'] = 'Username';
+$lang['view'] = 'View';
+$lang['updated'] = 'Updated';
+$lang['server'] = 'Panel';
+$lang['user'] = 'User';
+$lang['database'] = 'Database';
+$lang['edited'] = 'Edited';
+$lang['levels'] = 'Levels';
+$lang['notes'] = 'Notes';
+$lang['users'] = 'Users';
+$lang['stored'] = 'Stored';
+$lang['close'] = 'Close';
+$land['updateMessage'] = 'An update is available';
+$lang['update'] = 'Update';
+$lang['news'] = 'News';
+$lang['in'] = 'in';
+$lang['host'] = 'Host';
+$lang['user'] = 'User';
+$lang['visited'] = 'visited';
+$lang['debug'] = 'Debug';
+$lang['language'] = 'Language';
+$lang['failedUpdate'] = 'Failed Update';
+$lang['max'] = 'Max';
+$lang['current'] = 'Current';
+$lang['performance'] = 'Performance Mode';
+$lang['steamFound'] = 'Steam profile found:';
+$lang['vacban'] = 'VAC Banned';
+$lang['passreset'] = 'Password Reset';
+$lang['yourGang'] = 'Your Gang';
+$lang['topRich'] = 'Top Ten Richest Players';
