b9c2b595c0f8db0f82c24abc7d05743e7202741a
MightySCollins <theatrepro11@gmail.com>
2015-06-24 08:21:41 +0100

    All Files

---
+++ German.php
@@ -0,0 +1,172 @@
+<?php
+$lang = array();
+
+/*
+------------------------------------
+Common Items / Header Items
+------------------------------------
+*/
+
+$lang['welcome'] = 'Willkommen';
+$lang['players'] = 'Spieler';
+$lang['player'] = 'Spieler';
+$lang['vehicle'] = 'Fahrzeug';
+$lang['vehicles'] = 'Fahrzeuge';
+$lang['houses'] = 'Häuser';
+$lang['house'] = 'Haus';
+$lang['gangs'] = 'Gruppen';
+$lang['gang'] = 'Gruppe';
+$lang['wanted'] = 'Verbrechen';
+$lang['staff'] = 'Staff';
+$lang['overview'] = 'Übersicht';
+$lang['search'] = 'Suche';
+$lang['edit'] = 'Bearbeiten';
+$lang['editing'] = 'Bearbeitung';
+$lang['editor'] = 'Bearbeiter';
+$lang['members'] = 'Mitglieder';
+$lang['subChange'] = 'Änderungen speichern';
+$lang['editPlayer'] = 'Spieler bearbeiten';
+$lang['DELETE'] = 'Löschen';
+$lang['quickLook'] = 'Übersicht';
+$lang['PID'] = 'PID';
+$lang['curPlayers'] = 'Aktuelle Spieler';
+$lang['statOver'] = 'Statistik Überblick';
+$lang['overview'] = 'Übersicht';
+$lang['viewAll'] = 'Alle anzeigen';
+$lang['subChanges'] = 'Änderungen Speichern';
+$lang['yes'] = 'Ja';
+$lang['no'] = 'Nein';
+$lang['car'] = 'Auto';
+$lang['air'] = 'Luft';
+$lang['ban'] = 'Bann';
+$lang['uid'] = 'Datenbank ID';
+
+/*
+------------------
+errors
+------------------
+*/
+$lang['error'] = 'Error';
+$lang['dbError'] = 'Probleme bei der Verbindung zur Datenbank.';
+$lang['idNotSet'] = 'Keine ID vorhanden';
+$lang['noRes'] = 'Fehler - Keine Ergebnisse';
+$lang['noHouse'] = 'Besitzt kein Haus';
+$lang['noCar'] = 'Besitzt keine Autos';
+$lang['noPlayer'] = 'Spieler nicht gefunden';
+$lang['noPerm'] = 'Entschuldigung, du hast nicht die benötigte Rechte um die Seite zu öffnen';
+
+/*
+------------------
+Ranks
+------------------
+*/
+$lang['support'] = 'Support';
+$lang['mod'] = 'Moderator';
+$lang['administrator'] = 'Administrator';
+
+/*
+------------------
+Menu Items
+------------------
+*/
+
+$lang['navProfile'] = 'Profil';
+$lang['navAdmin'] = 'Admin';
+$lang['navNewUser'] = 'Neuen Benutzer hinzufügen';
+$lang['navDashboard'] = 'Startseite';
+$lang['navLogOut'] = 'Ausloggen';
+$lang['first'] = 'Erste';
+$lang['previous'] = 'Vorherige';
+$lang['next'] = 'Nächste';
+$lang['last'] = 'Letzte';
+
+/*
+----------------------------
+Table Headers / Buttons 
+---------------------------
+*/
+$lang['id'] = 'ID';
+$lang['staffName'] = 'Staff Name';
+$lang['emailAdd'] = 'Email-Addresse';
+$lang['rank'] = 'Level';
+$lang['name'] = 'Name';
+$lang['wantList'] = 'Fahnungsliste';
+$lang['aliases'] = 'Aliases';
+$lang['owner'] = 'Besitzer';
+$lang['maxMembers'] = 'Maximale Mitglieder';
+$lang['members'] = 'Mitglieder';
+$lang['bank'] = 'Konto';
+$lang['cash'] = 'Bargeld';
+$lang['cop'] = 'Polizist';
+$lang['police'] = 'Polizei';
+$lang['medic'] = 'Sanitäter';
+$lang['medics'] = 'Rettungsdienst';
+$lang['civil'] = 'Zivilisten';
+$lang['civ'] = 'Bürger';
+$lang['side'] = 'Fraktion';
+$lang['admin'] = 'Admin';
+$lang['donator'] = 'Donator';
+$lang['licenses'] = 'Lizenzen';
+$lang['gear'] = 'Ausrüstung';
+$lang['active'] = 'Aktiv';
+$lang['playerID'] = 'Spieler ID';
+$lang['wID'] = 'Spieler ID';
+$lang['bounty'] = 'Kopfgeld';
+$lang['crimes'] = 'Verbrechen';
+$lang['GUID'] = 'GUID';
+$lang['position'] = 'Position';
+$lang['owned'] = 'Eigentümer';
+$lang['inventory'] = 'Inventar';
+$lang['containers'] = 'Behälter';
+$lang['class'] = 'Klasse';
+$lang['type'] = 'Art';
+$lang['plate'] = 'Kennzeichen';
+$lang['alive'] = 'Intakt';
+$lang['colour'] = 'Farbe';
+$lang['class'] = 'Klasse';
+$lang['gutschein'] = 'Gutschein System';
+
+/*
+-----------------------
+SQL Phone Module
+-----------------------
+*/
+
+$lang['from'] = 'Von';
+$lang['to'] = 'An';
+$lang['message'] = 'Nachricht';
+$lang['time'] = 'Zeit';
+
+/*
+----------------------------
+	Need translation
+----------------------------
+*/
+$lang['setup'] = 'Die Datenbank wurde eingestellt';
+$lang['upgrade'] = 'Die Datenbank wurde geupdatet';
+$lang['Kick'] = 'Kick';
+$lang['topRich'] = 'Top 10 reichste Spieler';
+$lang['enterPass'] = 'Neues Passwort eingeben';
+$lang['password'] = 'Passwort';
+$lang['repeat'] = 'Wiederholen';
+$lang['login'] = 'Anmelden';
+$lang['cancel'] = 'Abbrechen';
+$lang['picture'] = 'Bild';
+$lang['changePass'] = 'Passwort ändern';
+$lang['gameServers'] = 'Game-Server';
+$lang['page'] = 'Seite';
+$lang['not'] = 'Keine';
+$lang['blacklisted'] = 'Blacklisted';
+$lang['arrested'] = 'Inhaftiert';
+$lang['finances'] = 'Finanzen';
+$lang['ship'] = 'Ship';
+$lang['noMessage'] = 'No messages';
+$lang['add'] = 'Add';
+$lang['new'] = 'New';
+$lang['note'] = 'Note';
+$lang['username'] = 'Username';
+$lang['update'] = 'An update is available';
+$lang['noRec'] = 'No records found';
+$lang['view'] = 'View';
+$lang['noNotes'] = 'No notes for this player';
+$lang['updated'] = 'Updated';
--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-# Cyberworks-Framework
+# Cyberworks Framework
 Although the Framework is still under construction we will slowly be releasing developer tools and documentation.
 You can sign up for our mailing list at http://eepurl.com/brit7P.
 
 ## Translating
-We have added many new strigns to our language files and would loke other developers to add to them before our release, editing is very simple and works like below:
+We have added many new strings to our language files and would like other developers to add to them before our release, editing is very simple and works like below:
     
     $lang['edit'] = 'Edit';
     
 This would be the English file and to translate into German all you need to do is change the words at the end so it becomes.
     
-    $lang['edit'] = 'bearbeiten'; (Ignore my german I failed it)
+    $lang['edit'] = 'bearbeiten'; (Ignore my German I failed it)
     
-We know there are lots of strings but are greatful that people what to give up there time to help us with this, if you need any help you can email cyberworks@cyberbyte.com or PM a developer on the Altis Life RPG forums.
+We know there are lots of strings but are grateful that people what to give up there time to help us with this, if you need any help you can email cyberworks@cyberbyte.com or PM a developer on the Altis Life RPG forums.
+### This branch is for user submissions and where pull requests should be done to. All files here are yet to be verified.
+All of the old string files have been included but it is advised edits are made to the new English file to avoid missing strings.
